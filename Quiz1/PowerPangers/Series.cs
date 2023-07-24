4b3fd82e0165f49d71edd5b4647a392077514d56
paulpv <paulpv@skbkontur.ru>
2023-07-24 16:57:00 +0500

    4

---
+++ Quiz1/PowerPangers/Series.cs
@@ -0,0 +1,65 @@
+﻿using System.Drawing;
+
+namespace Quiz1.PowerPangers;
+
+public class Series
+{
+    public void RunSeries()
+    {
+        var monster = new Monster();
+
+        var heroes = new[]
+        {
+            new Hero(Color.Blue),
+            new Hero(Color.Red),
+            new Hero(Color.Yellow),
+            new Hero(Color.Black),
+            new Hero(Color.Pink)
+        };
+        var heroCommand = new HeroCommand(heroes);
+
+        heroCommand.Kill(monster);
+
+        monster.SetBig();
+
+        heroCommand.GetZords();
+
+        heroCommand.Kill(monster);
+    }
+}
+
+public class Monster
+{
+    public void SetBig()
+    {
+    }
+}
+
+public class Hero
+{
+    private readonly Color _color;
+
+    public Hero(Color color)
+    {
+        _color = color;
+    }
+}
+
+public class HeroCommand
+{
+    private readonly Hero[] _command;
+    private Hero[] command;
+
+    public HeroCommand(Hero[] command)
+    {
+        _command = command;
+    }
+
+    public void Kill(Monster monster)
+    {
+    }
+
+    public void GetZords()
+    {
+    }
+}
