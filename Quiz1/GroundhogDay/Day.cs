d6bbc11f8d9f253aff961fea9a318f46d702fa00
paulpv <paulpv@skbkontur.ru>
2023-07-24 16:38:39 +0500

    3

---
+++ Quiz1/GroundhogDay/Day.cs
@@ -0,0 +1,29 @@
+﻿namespace Quiz1.GroundhogDay;
+
+public class Day
+{
+    private readonly Action _dayAction;
+
+    public Day(Action dayAction)
+    {
+        _dayAction = dayAction;
+    }
+
+    private void LiveDay()
+    {
+        while (true)
+            try
+            {
+                WakeUp();
+                _dayAction();
+                return;
+            }
+            catch
+            {
+            }
+    }
+
+    public void WakeUp()
+    {
+    }
+}
