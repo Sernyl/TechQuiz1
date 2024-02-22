67ae0cd78bd13ee1db7c9ccfc21fc87822218c0b
PaulPv <paulpv@skbkontur.ru>
2024-02-22 15:21:23 +0500

    pw

---
+++ Quiz1/PowerPangers/Series.java
@@ -0,0 +1,53 @@
+package Quiz1.PowerPangers;
+
+import java.awt.Color;
+
+public class Series {
+    public void runSeries() {
+        Monster monster = new Monster();
+
+        Hero[] heroes = {
+            new Hero(Color.BLUE),
+            new Hero(Color.RED),
+            new Hero(Color.YELLOW),
+            new Hero(Color.BLACK),
+            new Hero(Color.PINK)
+        };
+        HeroCommand heroCommand = new HeroCommand(heroes);
+
+        heroCommand.kill(monster);
+
+        monster.setBig();
+
+        heroCommand.getZords();
+
+        heroCommand.kill(monster);
+    }
+}
+
+class Monster {
+    public void setBig() {
+    }
+}
+
+class Hero {
+    private final Color color;
+
+    public Hero(Color color) {
+        this.color = color;
+    }
+}
+
+class HeroCommand {
+    private final Hero[] command;
+
+    public HeroCommand(Hero[] command) {
+        this.command = command;
+    }
+
+    public void kill(Monster monster) {
+    }
+
+    public void getZords() {
+    }
+}
