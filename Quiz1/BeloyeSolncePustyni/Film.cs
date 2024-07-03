4059c361101f5913a0fd09cf5aef131a7158033b
PaulPv <paulpv@skbkontur.ru>
2024-07-03 15:34:31 +0500

    Белое солнце

---
+++ Quiz1/BeloyeSolncePustyni/Film.cs
@@ -0,0 +1,35 @@
+﻿namespace Quiz1.BeloyeSolncePustyni
+{
+    public class Film
+    {
+        public void Do(Character character)
+        {
+            character.OpenFace();
+        }
+    }
+
+    public class Gulchitay : Character
+    {
+    }
+
+    public class Abdulla : Character
+    {
+        protected override void Open()
+        {
+            throw new ApplicationException();
+        }
+    }
+
+    public abstract class Character
+    {
+        public void OpenFace()
+        {
+            Open();
+        }
+
+        protected virtual void Open()
+        {
+
+        }
+    }
+}
