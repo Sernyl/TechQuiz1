e4ea2d627228a62e0755c02e226f326dacaaf14c
paulpv <paulpv@skbkontur.ru>
2025-09-05 13:43:12 +0500

    йц

---
+++ Quiz2/JakoneKiliana.cs
@@ -0,0 +1,37 @@
+﻿using Quiz2.KiSh;
+
+namespace Quiz2.JakoneKiliana;
+
+public class Singers
+{
+    public string GetFraze()
+    {
+        var I = new Human();
+        var You = new Human();
+
+        return new Knowledge(I, new Knowledge(You, new Knowledge(I, "You there"))).ToString();
+    }
+    
+    public class Knowledge
+    {
+        public Knowledge(Human maintainer, string knowledge) : this(maintainer, null, knowledge) { }
+        
+        public Knowledge(Human maintainer, Knowledge knowledge) : this(maintainer, knowledge, null) { }
+        
+        public Knowledge(Human maintainer, Knowledge? subknowledge, string? knowledge)
+        {
+            this.maintainer = maintainer;
+            this.subknowledge = subknowledge;
+            this.knowledge = knowledge;
+        }
+
+        private Knowledge? subknowledge;
+        private string? knowledge;
+        private Human maintainer;
+
+        public override string ToString()
+        {
+            return knowledge != null ? $"{maintainer} know that {knowledge}" : $"{maintainer} know that {subknowledge}";
+        }
+    }
+}
